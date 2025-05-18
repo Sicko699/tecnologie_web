924afd56771211affa6838fce2286d6f88f41b4e
Sicko699 <francesco.mattone.03@gmail.com>
2025-05-18 13:54:59 +0200

    Changes by fran5

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // Mostra la homepage (index.blade.php)
-        return view('index');
+        return view('home');
     }
 }
